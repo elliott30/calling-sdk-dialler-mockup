4c8339c54be1a6c66f9cd28c84dde79e7cc7f98b
Elliott Chapman <echapman@hubspot.com>
2023-02-28 13:54:39 +1300

    Dialler UI mockup draft one

--- src/pages/dialler.tsx
+++ src/pages/dialler.tsx
@@ -1,38 +1,75 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import React, { Component } from 'react';
+import Icon from '@mdi/react';
+import { mdiCircle, mdiDialpad, mdiMicrophoneOff } from '@mdi/js';
 
 export default function Home() {
   return (
     <>
+      <section className='section has-background-dark px-2 py-2 '>
+        <div className="columns is-vcentered is-mobile mb-0 ">
+          <div className="column">
+            <p className="has-text-white">Chris Cain</p>
+            <p className="has-text-white">+1 800 100 100</p>
+          </div>
+          <div className="column">
+            <button className="button is-danger is-pulled-right">End call</button>
+          </div>
+        </div>
+        <div className='buttons'>
+        </div>
+      </section>
+      <section className="section content has-background-light px-2 py-2">
+        <div className="columns has-text-centered	 is-vcentered is-mobile m-0">
+          <div className="column">
+            <button className="button is-rounded"><Icon path={mdiCircle}
+              title="Record"
+              size={1}
+            />
+            </button>
+            <p>Record</p>
+          </div>
+          <div className="column">
 
-      <span className="controls">
-        <div>Initialization/Login</div>
-        <input type="button" value="initialized" />
-        <input type="button" value="log in" />
-        <input type="button" value="log out" />
-        <hr />
-        <div>Calling related methods</div>
-        <input type="button" value="outgoing call started" />
-        <input type="button" value="call answered" />
-        <br />
-        <input type="button" value="call ended" />
-        <input type="button" value="call completed" />
-        <hr />
-        <div>Misc. events</div>
-        <input type="button" value="send error" />
-        <input type="button" value="change size" />
-      </span>
-      <hr />
-      <div>Incoming Messages</div>
-      <div id="incomingMsgs"></div>
+            <button className="button is-rounded"><Icon path={mdiMicrophoneOff}
+              title="Record"
+              size={1}
+            /></button>
+            <p>Mute</p>
 
-      <script src="./bin/index_combined.js" async></script>
+          </div>
+          <div className="column">
+
+            <button className="button is-rounded"><Icon path={mdiDialpad}
+              title="Record"
+              size={1}
+            /></button>
+            <p>Keypad</p>
+
+          </div>
+        </div>
+      </section>
+      <section className='section content py-0'>
+        <form>
+          <textarea className="textarea" placeholder="e.g. Hello world"></textarea>
+        </form>
+      </section>
+
+      <section className='section content '>
+        <p className="">Debugging Events</p>
+        <div className='buttons'>
+          <button className="button">initialized</button>
+          <button className="button">log in</button>
+          <button className="button">log out</button>
+          <button className="button">outgoing call started</button>
+          <button className="button">call answered</button>
+          <button className="button">call ended</button>
+          <button className="button">call completed</button>
+          <button className="button">send error</button>
+          <button className="button">change size</button>
+        </div>
 
 
+      </section>
     </>
   )
 }
